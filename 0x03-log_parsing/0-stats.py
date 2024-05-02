5d900ec5e85754fe81a2184ae2b878a30d05f2cc
Abdullah <abdallahmosbah25@gmail.com>
2024-05-03 02:11:35 +0300

    change the planv2

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""read log file"""
+"""reads stdin,compute metrics"""
+import re
 
 
-def inp_getter(lines):
-    """ get the inp """
+def get_input(lines):
+    """gets target info from input."""
     line = lines.split(' ')
     return {
         'status_code': line[-2],
@@ -11,17 +12,17 @@ def inp_getter(lines):
     }
 
 
-def stats(file_size, status_code):
-    """ get the stats """
-    print('File size: {:d}'.format(file_size), flush=True)
-    for status_code in sorted(status_code.keys()):
-        n = status_code.get(status_code, 0)
+def print_stats(file_size_sum, status_codes_stats):
+    """Print acumulated statistics of HTTP log"""
+    print('File size: {:d}'.format(file_size_sum), flush=True)
+    for status_code in sorted(status_codes_stats.keys()):
+        n = status_codes_stats.get(status_code, 0)
         if n > 0:
             print('{:s}: {:d}'.format(status_code, n), flush=True)
 
 
-def main():
-    """ main """
+def run():
+    """run input parser"""
     line_n = 0
     file_size_sum = 0
     status_codes_stats = {
@@ -37,17 +38,17 @@ def main():
     try:
         while True:
             line = input()
-            line_info = inp_getter(line)
+            line_info = get_input(line)
             code = line_info.get('status_code', '0')
             if code in status_codes_stats.keys():
                 status_codes_stats[code] += 1
             file_size_sum += line_info['file_size']
             line_n += 1
             if line_n % 10 == 0:
-                stats(file_size_sum, status_codes_stats)
+                print_stats(file_size_sum, status_codes_stats)
     except (KeyboardInterrupt, EOFError, SystemExit):
-        stats(file_size_sum, status_codes_stats)
+        print_stats(file_size_sum, status_codes_stats)
 
 
 if __name__ == '__main__':
-    main()
+    run()
