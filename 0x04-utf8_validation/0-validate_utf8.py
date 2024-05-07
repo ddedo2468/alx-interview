2841c2aa9a957ed8dce2814c161f1292256ad54a
Abdullah <abdallahmosbah25@gmail.com>
2024-05-07 18:31:17 +0300

    fix 01

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,29 +3,23 @@
 
 
 def validUTF8(data):
-    """return true if valid utf-8 stream"""
+    """Determines if a given data set represents a valid UTF-8 encoding"""
     num_of_bytes = 0
-
-    for value in data:
-        if num_of_bytes > 0:
-            if value >> 6 != 0b10:
-                return False
-                num_of_bytes -= 1
-
+    for char in data:
+        bin = format(char, "#010b")[-8:]
+        if num_of_bytes == 0:
+            if bin.startswith("0"):
+                continue
+            elif bin.startswith("110"):
+                num_of_bytes = 1
+            elif bin.startswith("1110"):
+                num_of_bytes = 2
+            elif bin.startswith("11110"):
+                num_of_bytes = 3
             else:
-                if value >> 7 == 0:
-                    num_of_bytes = 0
-
-                elif value >> 5 == 0b110:
-                    num_of_bytes = 1
-
-                elif value >> 4 == 0b1110:
-                    num_of_bytes = 2
-
-                elif value >> 3 == 0b11110:
-                    num_of_bytes = 3
-
-                else:
-                    return False
-
-        return num_of_bytes == 0
+                return False
+        else:
+            if not bin.startswith("10"):
+                return False
+            num_of_bytes -= 1
+    return num_of_bytes == 0
