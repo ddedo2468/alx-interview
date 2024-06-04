cfb136451c7f5a708d2adf841d4d05e949fd346d
Abdullah <abdallahmosbah25@gmail.com>
2024-06-04 23:54:09 +0300

    greedy algo

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,15 +4,16 @@
 
 def makeChange(coins, total):
     """consider it as a useful documentation idk"""
+
     if total <= 0:
         return 0
 
-    dp = [total + 1] * (total + 1)
+    coins.sort(reverse=True)
+    dp = [float("inf")] * (total + 1)
     dp[0] = 0
-
     for i in range(1, total + 1):
         for coin in coins:
             if coin <= i:
                 dp[i] = min(dp[i], dp[i - coin] + 1)
 
-    return dp[total] if dp[total] != total + 1 else -1
+    return dp[total] if dp[total] != float("inf") else -1
