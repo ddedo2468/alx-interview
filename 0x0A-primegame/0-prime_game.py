55fd63d0d2d4dc52a1e4c2793617c2596ba6c397
Abdullah <abdallahmosbah25@gmail.com>
2024-06-16 01:12:52 +0300

    pygame

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+prime time
+"""
+
+
+def isWinner(x, nums):
+    """
+    main fnction for prime game module
+    """
+    maria = 0
+    ben = 0
+
+    def SieveOfEratosthenes(n):
+        """Sieve of Eratosthenes Function"""
+
+        prime = [True for i in range(n + 1)]
+        pointer = 2
+        while pointer * pointer <= n:
+            if prime[pointer] == True:
+                for i in range(pointer * pointer, n + 1, pointer):
+                    prime[i] = False
+            pointer += 1
+        prime[0] = False
+        prime[1] = False
+        return prime
